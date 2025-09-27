edf7bf3325d997933a0b400f54c02ad71ec82d8b
Mark Juan <118956480+markjuan15@users.noreply.github.com>
2025-09-27 23:23:26 +0800

    basename

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from 'react-router'
 import Home from './pages/home'
-import NotFound from './pages/components/notFound'
+// import NotFound from './pages/components/notFound'
 
 function App() {
 
   return (
     <Routes>
-      <Route path="*" element={<NotFound />}></Route>
+      {/* <Route path="*" element={<NotFound />}></Route> */}
       <Route path="/" element={<Home />}></Route>
       <Route path="/home" element={<Home />}></Route>
     </Routes>

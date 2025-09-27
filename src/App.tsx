cc0c91de8c6499b49ca44785c1f3150ff43e27e7
Mark Juan <118956480+markjuan15@users.noreply.github.com>
2025-09-27 22:32:02 +0800

    route

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import { Route, Routes } from 'react-router'
-import { BrowserRouter } from 'react-router-dom'
 import Home from './pages/home'
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="*" element={<NotFound />}></Route> */}
-        <Route path="/" element={<Home />}></Route>
-        {/* <Route path="/home" element={<Home />}></Route> */}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {/* <Route path="*" element={<NotFound />}></Route> */}
+      <Route path="/" element={<Home />}></Route>
+      {/* <Route path="/home" element={<Home />}></Route> */}
+    </Routes>
   )
 }
 

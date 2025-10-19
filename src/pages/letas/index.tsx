05f8d45c99da14ce5f4bfa8fd2345d4a4908b92d
Mark Juan <118956480+markjuan15@users.noreply.github.com>
2025-10-19 21:04:36 +0800

    pdf 6

--- src/pages/letas/index.tsx
+++ src/pages/letas/index.tsx
@@ -12,7 +12,7 @@ export default function Letas({ index }: Iprops) {
 
     return (
         <motion.div viewport={{ amount: 0.8 }} onViewportEnter={(entry: any) => toggleTabState(parseInt(entry.target.dataset.value))} id="letas" data-value={index} className="h-[calc(100vh-2.8rem)] snap-start snap-always">
-            <div className="flex flex-col items-center w-full h-full gap-[2rem] overflow-auto p-[2rem] no-scrollbar">
+            <div className="flex flex-col items-center w-full h-full gap-[2rem] overflow-auto no-scrollbar">
                 <PDFViewer file={letas} />
             </div>
         </motion.div>
